b44066beec8aa6500511513a4e2a67f707b2931a
Joro Maglalang <johnroger.maglalang@gmail.com>
2016-11-06 22:14:13 +0800

    Add DbContext Class
    Add DbContext Class

---
+++ FishDAL/FishContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using FishCore.Objects;
+
+namespace FishDAL
+{
+    public class FishContext : DbContext
+    {
+        public DbSet<Account> Accounts {get; set;}
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+
+    }
+}
