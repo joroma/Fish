d99bedf0c6fdc2b612163461b890691fa75f27da
Joro Maglalang <johnroger.maglalang@gmail.com>
2016-11-06 21:51:24 +0800

    Add initial test entities
    Add initial test entities

---
+++ FishCore/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishCore
+{
+    public interface Entity
+    {
+        int ID { get; set; }
+        string Description { get; set; }
+        DateTime CreationDate { get; set; }
+        string CreationOperator { get; set; }
+        DateTime UpdateDate { get; set; }
+        string UpdateOperator { get; set; }
+
+        void AssignCreateDate();
+
+    }
+}
---
+++ FishCore/Objects/ExpenseType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FishCore;
+
+namespace FishCore.Objects
+{
+    public class ExpenseType
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+
+        public decimal BudgetAmount { get; set; }
+        
+
+    }
+}
