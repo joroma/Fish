6e1592a91054b1e7922114f1df97c9a6e6c0dc18
Joro Maglalang <johnroger.maglalang@gmail.com>
2016-11-06 22:05:32 +0800

    Add transaction models
    Add transaction models

--- FishCore/Objects/Account.cs
+++ FishCore/Objects/Account.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FishCore;
 
 namespace FishCore.Objects
 {
-    public class ExpenseType
+    public class Account
     {
         public int ID { get; set; }
         public string Description { get; set; }
 
-        public decimal BudgetAmount { get; set; }
-
-        public Account Account { get; set; }
-        
     }
 }
---
+++ FishCore/Objects/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishCore.Objects
+{
+    public class Transaction
+    {
+        public int ID { get; set; } 
+        public string Descriptio { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Trandate { get; set; }
+
+        public  ExpenseType expenseType { get; set; }
+
+
+    }
+}
