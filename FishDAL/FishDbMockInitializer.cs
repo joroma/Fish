fb10dfeec73beac0aa21e8c6761385e0308d90b6
Joro Maglalang <johnroger.maglalang@gmail.com>
2016-11-06 22:16:42 +0800

    Add mock initializer
    Add mock initializer

---
+++ FishDAL/FishDbMockInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FishDAL
+{
+    public class FishDbMockInitializer : DropCreateDatabaseIfModelChanges<FishContext>
+    {
+        protected override void Seed(FishContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
