4aa018ae116025d61ce5c693d1cf207b6e91be59
Joro Maglalang <johnroger.maglalang@gmail.com>
2016-11-06 22:17:36 +0800

    Add account type to classify account type codes
    Add account type

---
+++ FishCore/Objects/AccountType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishCore.Objects
+{
+    public class AccountType
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
