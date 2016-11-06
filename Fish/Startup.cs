6dee64385b8176834b615febff1cb389b8cff8fd
Joro Maglalang <johnroger.maglalang@gmail.com>
2016-11-06 21:05:33 +0800

    Add project files.

---
+++ Fish/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fish.Startup))]
+namespace Fish
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
